b4859cbe67d159e338326c46ad3a0364aa0c638a
Deepak Kumar <mahtodeepak857@gmail.com>
2024-01-30 01:47:40 +0530

    added Throttling

---
+++ Throttling/throttle.js
@@ -0,0 +1,22 @@
+const container = document.querySelector(".container");
+
+const throttle = (callback, delay) => {
+  let isWaiting = false;
+  return (...args) => {
+    if (isWaiting) return;
+    callback(...args);
+    isWaiting = true;
+    setTimeout(() => {
+      isWaiting = false;
+    }, delay);
+  };
+};
+
+const handleScroll = () => {
+  console.log("scrolled");
+  //   logic of API
+};
+
+const throttledScroll = throttle(handleScroll, 500);
+
+container.addEventListener("scroll", throttledScroll);
